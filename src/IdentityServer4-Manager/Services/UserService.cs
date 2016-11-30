20bf3a9343227bdd979d5060d1bf5a5e899ae904
wu-xian <270084782@qq.com>
2016-11-30 22:35:29 +0800

    commit UserService

--- src/IdentityServer4-Manager/Services/UserService.cs
+++ src/IdentityServer4-Manager/Services/UserService.cs
@@ -3,6 +3,8 @@ using IdentityServer4_Manager.Exceptions;
 using IdentityServer4_Manager.Extension;
 using IdentityServer4_Manager.Model.ViewModel;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,10 +15,10 @@ namespace IdentityServer4_Manager.Services
     public class UserService
     {
         private readonly UserManager<Model.IdentityUser> _userManager;
-        private readonly RoleManager<Microsoft.AspNetCore.Identity.EntityFrameworkCore.IdentityRole> _roleManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
 
         public UserService(UserManager<Model.IdentityUser> userManager,
-            RoleManager<Microsoft.AspNetCore.Identity.EntityFrameworkCore.IdentityRole> roleManager)
+            RoleManager<IdentityRole> roleManager)
         {
             _userManager = userManager;
             _roleManager = roleManager;
@@ -33,22 +35,22 @@ namespace IdentityServer4_Manager.Services
             return await _userManager.CreateAsync(user);
         }
 
-        public async Task<PagingResponse> GetDisplayUser(PagingRequest pagingRequest, string userId, string userName)
+        public async Task<PagingResponse> GetDisplayUsers(PagingRequest pagingRequest, string userId, string userName)
         {
             int totalCount = 0;
-            var users = _userManager.Users
+            var users = await _userManager.Users
                     .Paged(
                     d =>
                         (string.IsNullOrEmpty(userId) || d.Id == userId) &&
                         (string.IsNullOrEmpty(userName) || d.UserName.Contains(userName)),
-                    pagingRequest.Order,
-                    pagingRequest.Offset,
-                    pagingRequest.Limit,
-                    pagingRequest.isAsc,
-                    ref totalCount
-            )
-            .Select(d => Mapper.Map<UserDisplay>(d))
-            .ToList();
+                        pagingRequest.Order,
+                        pagingRequest.Offset,
+                        pagingRequest.Limit,
+                        pagingRequest.isAsc,
+                        ref totalCount
+                        )
+                    .Select(d => Mapper.Map<UserDisplay>(d))
+                    .ToListAsync();
             return new PagingResponse()
             {
                 Total = totalCount,
@@ -67,7 +69,7 @@ namespace IdentityServer4_Manager.Services
             var usr = await _userManager.FindByIdAsync(userId);
             if (usr == null)
             {
-                throw new ParamsWrongException(nameof(usr));
+                throw new ParamsWrongException(nameof(userId));
             }
             var userCurrentRoles = await _userManager.GetRolesAsync(usr);
             var removeRoles = userCurrentRoles.Except(roles).ToList();
@@ -75,6 +77,38 @@ namespace IdentityServer4_Manager.Services
             var removeResult = await _userManager.RemoveFromRolesAsync(usr, removeRoles);
             var addResult = await _userManager.AddToRolesAsync(usr, addRoles);
             return removeResult.Succeeded || addResult.Succeeded;
+        }
+
+        public async Task<IdentityResult> CreateUser(string userName, string password, string email, string phoneNumber)
+        {
+            var usr = new Model.IdentityUser()
+            {
+                UserName = userName,
+                Email = email,
+                PhoneNumber = phoneNumber
+            };
+            var result = await _userManager.CreateAsync(usr);
+            _userManager.AddPasswordAsync(usr, password);
+            return result;
+        }
+
+        public async Task<IdentityResult> CreateUserClaims(string userId, IEnumerable<Model.Claim> claims)
+        {
+            var userClaims = claims.Select(d => Mapper.Map<IdentityUserClaim<int>>(d).ToClaim()).ToList();
+            var usr = await _userManager.FindByIdAsync(userId);
+            if (usr == null)
+                throw new ParamsWrongException(nameof(userId));
+            return await _userManager.AddClaimsAsync(usr, userClaims);
+        }
+
+        public async Task<IdentityResult> RemoveUserClaims(string userId, Dictionary<string,string> claims)
+        {
+            var usr = await _userManager.FindByIdAsync(userId);
+            if (usr == null)
+                throw new ParamsWrongException(nameof(userId));
+            var removeClaims = (await _userManager.GetClaimsAsync(usr))
+                .Where(d=>d.ValueType==;
+            await _userManager.RemoveClaimsAsync(usr,)
         }
     }
 }
